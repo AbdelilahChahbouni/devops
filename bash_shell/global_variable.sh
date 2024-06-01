902db6f71757174b6df355628af2bac534cb16f1
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-06-01 16:31:32 +0100

    update repo

---
+++ bash_shell/global_variable.sh
@@ -0,0 +1,9 @@
+echo "local variable is $var1"
+
+echo "global variable is $var2"
+
+echo "$var2"
+
+echo '$var2'
+
+echo \$var2  
---
+++ bash_shell/local_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Print Local Variable
+
+name=Reda
+age=20
+
+echo "this is $name your age is $age and $data"
+
