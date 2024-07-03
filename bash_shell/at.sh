78a251bcb503b1a473cabdf026cabe5629bf4343
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-07-03 20:58:53 +0100

    update repo

---
+++ bash_shell/at.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "the script is running"
+mkdir test
+echo "the folder creted"
+cd test
+echo "change ditrectory to test"
+touch file1
+echo "the file is creted"
+echo "hello from at file" > test
+echo "the script is done"
+
+
+
